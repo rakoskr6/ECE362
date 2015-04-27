2b8d040219b8988c2fff352d569f2be2d940b31f
Alex Marcellus <alex.marcellus@outlook.com>
2015-04-27 17:07:08 -0400

    More Compass Code
    Added more utility functions. Completed heading and magnitude reception

--- OtherCode/Compass/Sources/main.c
+++ OtherCode/Compass/Sources/main.c
@@ -64,6 +64,7 @@
 #include "derivative.h"      /* derivative-specific definitions */
 #include <mc9s12c32.h>
 #include <float.h>
+#include <math.h>
 
 /* All functions after main should be initialized here */
 char inchar(void);
@@ -76,14 +77,16 @@ char transfer(char x);
 
 //Compass Utility Functions
 char compassReadByte(char x);
+void compassWriteByte(char address, char data);
+void compassReadBytes(char address, char * dest, char count);
+void readMag();
+double getHeading(double xMag, double yMag);
 
 
 /* Variable declarations */
 double mRes = 2.0 / 32768.0;
-char xmtest;
-char xmtest2;
-char WHOAMI;
-int i;
+char mx; char my; char mz;
+char check;
    	   			 		  			 		       
 
 /* Special ASCII characters */
@@ -104,7 +107,19 @@ int i;
 #define LINE2 = 0xC0	// LCD line 2 cursor position
 
 /* COMPASS REGISTERS */
+#define COMPCS PTT_PTT2
 #define WHO_AM_I_XM 0x0F //Check register
+#define CTRL_REG0_XM 0x1F
+#define CTRL_REG1_XM 0x20
+#define CTRL_REG2_XM 0x21
+#define CTRL_REG3_XM 0x22
+#define CTRL_REG4_XM 0x23
+#define CTRL_REG5_XM 0x24
+#define CTRL_REG6_XM 0x25
+#define CTRL_REG7_XM 0x26
+#define INT_CTRL_REG_M 0x12
+#define OUT_X_L_M 0x08
+#define PI 3.14159
 
 
 	 	   		
@@ -142,9 +157,17 @@ void  initializations(void) {
   SPICR1 = 0x1C; //master, no interrupt, idle high, sample at odd edges, off initially
   SPICR2 = 0; //normal, non-bidirection
   SPIBR = 0x12; //Baud rate to 6.0 Megabits/second
-  // Compass
-  DDRT = 0x80;
-  PTT_PTT7 = 1;
+  
+  // Compass Initialization
+  DDRT = 0x04;
+  COMPCS = 1;
+  compassWriteByte(CTRL_REG0_XM, 0x00);
+  compassWriteByte(CTRL_REG1_XM, 0x00);
+  compassWriteByte(CTRL_REG5_XM, 0x94);
+  compassWriteByte(CTRL_REG6_XM, 0x00);
+  compassWriteByte(CTRL_REG7_XM, 0x00);
+  compassWriteByte(CTRL_REG4_XM, 0x04);
+  compassWriteByte(INT_CTRL_REG_M, 0x09);
   
             
 /* Initialize interrupts */
@@ -162,11 +185,9 @@ void main(void) {
   	DisableInterrupts;
 	initializations(); 		  			 		  		
 	EnableInterrupts;
-	WHOAMI = 0x0F;
-	WHOAMI = WHOAMI | 0x80;
-	xmtest = compassReadByte(WHOAMI);
-	xmtest2 = compassReadByte(0x20);
-
+  check = compassReadByte(WHO_AM_I_XM);
+  readMag();
+  getHeading((double) mx, (double) my);
  for(;;) {
   
 /* < start of your main loop > */ 
@@ -207,14 +228,60 @@ COMPASS UTILITY FUNCTIONS
 char compassReadByte(char x){
   char returnChar;
   x |= 0x80; //MSB for compass reads is 1
-  PTT_PTT7 = 0;
+  COMPCS = 0;
   SPICR1 |= 0x40; //Enables SPI
   transfer(x); //Sends the instruction to compass
   returnChar = transfer(0x00); //Reads the result of instruction
   SPICR1 &= 0xBF; //Disables SPI
-  PTT_PTT7 = 1;
+  COMPCS = 1;
   return returnChar;
-}   
+}
+
+void compassWriteByte(char address, char data){
+  address &= 0x3F; //Only the last six bits are valid for write
+  COMPCS = 0;
+  SPICR1 |= 0x40; //Enables SPI
+  transfer(address); //Gives the compass the address
+  transfer(data); //Gives the compass the data to write
+  SPICR1 &= 0xBF; //Disables SPI
+  COMPCS = 1;
+}
+
+void compassReadBytes(char address, char * dest, char count){
+  int i;
+  COMPCS = 0;
+  SPICR1 |= 0x40; //Enables SPI
+  address &= 0x3F; //Only last six bits are valid address
+  address |= 0xC0; //First two bits on for read and multibyte
+  transfer(address); //First two bits on
+  for(i = 0; i < count; i++){
+    dest[i] = transfer(0x00); //Read into destination array
+  }
+  SPICR1 &= 0xBF; //Disables SPI
+  COMPCS = 1;
+}
+
+void readMag(){
+  char temp[6];
+  compassReadBytes(OUT_X_L_M, temp, 6);
+  mx = (temp[1] << 8) | temp[0];
+  my = (temp[3] << 8) | temp[2];
+  mz = (temp[5] << 8) | temp[4];
+}
+
+double getHeading(double xMag, double yMag){
+  double heading;
+  if(yMag > 0){
+    heading = 90.0 - (atan(xMag/yMag) * (180/PI));
+  } else if(yMag < 0){
+    heading = -(atan(xMag/yMag) * (180/PI));
+  } else{
+    if(xMag < 0) heading = 180;
+    else heading = 0;
+  }
+  return heading;
+}
+  
 
 
 /*
